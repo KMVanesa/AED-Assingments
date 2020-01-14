a560b1d451663ed8c21ff45d70a4d2f79aebc3c1
Krutarth Vanesa <vanesa.k@husky.neu.edu>
2020-01-14 15:02:27 -0500

    Lab Example

---
+++ LabExample/src/Business/product.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author chief_kmv
+ */
+public class product {
+    private String f_name;
+    private String l_name;
+    private String p_num;
+    private String dob;
+    private String age;
+    private int height;
+    private int weight;
+    private String ssn;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    public String getP_num() {
+        return p_num;
+    }
+
+    public void setP_num(String p_num) {
+        this.p_num = p_num;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+   
+}
